5a61391ccf374bdb8d2768f1ce8cfa57e2d657a1
akhil1672 <33376520+akhil1672@users.noreply.github.com>
2018-02-02 21:38:54 +0530

    Changed the tests

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const app=express();
 const cors=require('cors');
 const body=require('body-parser');
 const path = require('path');
+// const db=require('./src/config/database');
+
+// async function connectdb(){
+//     let database=await db();
+// }
+
+// connectdb();
 
 const home=require('./src/routes/home.js');
 const routes=require('./src/routes/routes.js');
@@ -14,10 +21,10 @@ app.use(body.urlencoded({extended:false}));
 //app.use(express.static(path.join(__dirname,'dist')));
 
 app.use('/',home);
-app.use('/api/',routes);
-
-app.listen(process.env.PORT || 3000,()=>{
-    console.log("Server started");
+app.use('/api',routes);
+port=3000;
+app.listen(process.env.PORT || port,()=>{
+    console.log("Server started "+port);
 })
 
 module.exports=app;
--- test/routes/routes.test.js
+++ test/routes/routes.test.js
@@ -1,48 +1,63 @@
 const chai = require('chai');
-const chaihttp = require('chai-http');
+//const chaihttp = require('chai-http');
 let expect = require('chai').expect;
-const request=require('request');
-let server = require('../../index');
+const request=require('supertest');
+const server = require('../../index.js');
 let should = chai.should();
 
-chai.use(chaihttp);
+chai.use(require('chai-http'));
 
 describe('Movies', () => {
-    describe('/GET movies', () => {
+    //this.timeout(5000);
+    describe('/GET movies',() => { 
+        before(function () {
+            this.timeout(5000);
+        })
+        //this.timeout(5000);
         it('should get all the movies',async () => {
-            chai.request(server).get('/movies').end((err, res) => {
-                    console.log(res);
-                    res.should.have.status(404);
-                    res.body.should.be.a('array');
-                    res.body.length.should.be.eql(0);
-                    expect('Content-Length').to.be.eql(145);
-                    res.body[0].should.have.property('_id');
-                    done();
-                });
+            let res1 = {};
+            
+            res=await request(server).get('/api/movies');
+            console.log(res);
+                expect(res1).to.have.status(200);
+                expect(res1.body[0].name).to.be.equal('Hulk 2');
+        
+          
+            // expect(res).to.have.status(200);
+            // expect(res.body[0].name).to.be.equal('Hulk 2');
+            // chai.request(server).get('/api/movies').end((err, res) => {
+            //         //console.log(res);
+            //         res.should.have.status(404);
+            //         res.body.should.be.a('array');
+            //         res.body.length.should.be.eql(0);
+            //         expect('Content-Length').to.be.eql(145);
+            //         res.body[0].should.have.property('_id');
+            //         done();
+            //    });
         })
     })
 
-    describe('/GET movies',()=>{
-        it('should return all movies',()=>{
-            request('http://localhost:3000/api/movies',(err,res,body)=>{
-                res.should.have.status(200);
-                res.body.should.be.a('array');
-                done();
-            })
-        })
-    })
+    // describe('/GET movies',()=>{
+    //     it('should return all movies',()=>{
+    //         request('http://localhost:3000/api/movies',(err,res,body)=>{
+    //             res.should.have.status(200);
+    //             res.body.should.be.a('array');
+    //             done();
+    //         })
+    //     })
+    // })
 
-    describe('/POST movies', () => {
-        it('should add movie', async () => {
-            chai.request(server).post('/movies').send({name:'Dogs',year:1970,rating:8.0}).end((err, res) => {
-                //console.log(res);
-                res.should.have.status(200);
-                res.body.should.be.a('array');
-                res.body.should.be.json;
-                done();
-            });
-        })
-    })
+    // describe('/POST movies', () => {
+    //     it('should add movie', async () => {
+    //         chai.request(server).post('/api/movies').send({name:'Dogs',year:1970,rating:8.0}).end((err, res) => {
+    //             //console.log(res);
+    //             res.should.have.status(200);
+    //             res.body.should.be.a('array');
+    //             res.body.should.be.json;
+    //             done();
+    //         });
+    //     })
+    // })
 
 
 })
